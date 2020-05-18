c69251dc48e2abee6bb819222ca3a3915fe736b8
dylanirlbeck <dylanirlbeck@gmail.com>
2020-05-17 22:39:54 -0500

    Temporary font weight fix for Chrome
    Fixes #3 --- related to https://github.com/tailwindcss/tailwindcss/issues/1402

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -1,7 +1,22 @@
 module.exports = {
   theme: {
     extend: {},
+    fontFamily: {
+      sans: [
+        "-apple-system",
+        '"Segoe UI"',
+        '"Roboto"',
+        '"Oxygen"',
+        '"Ubuntu"',
+        '"Cantarell"',
+        '"Fira Sans"',
+        '"Droid Sans"',
+        "'Inter'",
+        '"Helvetica Neue"',
+        "sans-serif"
+      ]
+    }
   },
   variants: {},
-  plugins: [],
-}
+  plugins: []
+};
